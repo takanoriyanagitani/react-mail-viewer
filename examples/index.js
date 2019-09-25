fe6625885b7ce52c1c46eccbd0b7fdd6be5aeee5
takanoriyanagitani <takanori.yanagitani@gmail.com>
2019-09-26 05:11:24 +0900

    example using ReactMailViewer

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,11 @@
+import { ReactMailViewer } from "lib/index.js"
+
 class ReactMailViewer__Example1 extends React.PureComponent {
   render(){
-    return React.createElement("div", {}, "hw this is ex 1.")
+    return React.createElement("div", {}, [
+      React.createElement("div", {key: 0}, "this is div."),
+      React.createElement("div", {key: 1}, React.createElement(ReactMailViewer)),
+    ])
   }
 }
 
