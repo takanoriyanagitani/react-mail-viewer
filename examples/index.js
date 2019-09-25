b5be6362e724bcc2718964bbca5c7a65948113fe
takanoriyanagitani <takanori.yanagitani@gmail.com>
2019-09-26 05:01:51 +0900

    example index page loads React from unpkg.

---
+++ examples/index.js
@@ -0,0 +1,7 @@
+class ReactMailViewer__Example1 extends React.PureComponent {
+  render(){
+    return React.createElement("div", {}, "hw this is ex 1.")
+  }
+}
+
+ReactDOM.render(React.createElement(ReactMailViewer__Example1), document.getElementById("react1"))
