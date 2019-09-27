619e64dfd5fbe448ce8125d605b0e15f1a8e0de9
takanoriyanagitani <takanori.yanagitani@gmail.com>
2019-09-28 06:34:52 +0900

    planning to sort mails by sender or date.

--- examples/index.js
+++ examples/index.js
@@ -1,21 +1,9 @@
 import { ReactMailViewer } from "../lib/index.js"
 
-const {
-  Container,
-  Row,
-  Col,
-} = window.Reactstrap
-
 class ReactMailViewer__Example1 extends React.PureComponent {
   render(){
     return React.createElement(Container, {}, [
-      React.createElement(Row, { key: "r0" }, [
-        React.createElement(Col, { key: "c0", xs: 12 }, "xs-12"),
-        React.createElement(Col, { key: "c1", sm:  6 }, "sm-06"),
-        React.createElement(Col, { key: "c2", md:  4 }, "md-04"),
-        React.createElement(Col, { key: "c3", lg:  2 }, "lg-02"),
-        React.createElement(Col, { key: "c4", xl:  1 }, "xl-01"),
-      ]),
+      React.createElement(ReactMailViewer, { key: "rmv" }),
     ])
   }
 }
