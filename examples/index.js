c8c1cd9f93dcd350dc68e5b794a9ae3226d83d4f
takanoriyanagitani <takanori.yanagitani@gmail.com>
2019-09-28 05:10:58 +0900

    xs,sm,md,lg,xl test

--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,21 @@
 import { ReactMailViewer } from "../lib/index.js"
 
+const {
+  Container,
+  Row,
+  Col,
+} = window.Reactstrap
+
 class ReactMailViewer__Example1 extends React.PureComponent {
   render(){
-    return React.createElement("div", {}, [
-      React.createElement("div", {key: 0}, "this is div."),
-      React.createElement("div", {key: 1}, React.createElement(ReactMailViewer)),
+    return React.createElement(Container, {}, [
+      React.createElement(Row, { key: "r0" }, [
+        React.createElement(Col, { key: "c0", xs: 12 }, "xs-12"),
+        React.createElement(Col, { key: "c1", sm:  6 }, "sm-06"),
+        React.createElement(Col, { key: "c2", md:  4 }, "md-04"),
+        React.createElement(Col, { key: "c3", lg:  2 }, "lg-02"),
+        React.createElement(Col, { key: "c4", xl:  1 }, "xl-01"),
+      ]),
     ])
   }
 }
