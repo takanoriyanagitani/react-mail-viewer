f03c2d563fbd736da5d1b3a57acdef83f9e96cad
takanoriyanagitani <takanori.yanagitani@gmail.com>
2019-10-18 19:28:10 +0900

    commit before major changes...

--- examples/index.js
+++ examples/index.js
@@ -1,22 +1,25 @@
-import { ReactMailViewer } from "../lib/index.js"
+import { Mails } from "../lib/react-mail-viewer.js"
 
 class ReactMailViewer__Example1 extends React.PureComponent {
   render(){
     return React.createElement("div", {}, [
-      React.createElement(ReactMailViewer, {
-        key: "rmv",
-        data: {
-          reload: () => {},
-          date: {
-            year:  (lbi, ubi) => [],
-            month: year       => [],
-            day:   month      => [],
-            list:  day        => [],
-            mail:  (list, i)  => {},
+      React.createElement(Mails, {
+        mails: [
+          {
+            from:    { alias: "G", name: "Google",              address: "no-reply@accounts.google.com"  },
+            to:      { alias: "T", name: "Takanori Yanagitani", address: "takanori.yanagitani@gmail.com" },
+            subject: "Security notification",
+            body:    React.createElement("div", {}, "hw this is body"),
+            date:    "Fri, 11 Oct 2019 19:53:06 +0000 (GMT)",
           },
-          from: query => [],
-          to:   query => [],
-        },
+          {
+            from:    { alias: "A", name: "Apple",               address: "just_for_you@giftcards.apple.com" },
+            to:      { alias: "T", name: "Takanori Yanagitani", address: "takanori.yanagitani@gmail.com"    },
+            subject: "Gift card sent",
+            body:    React.createElement("div", {}, "tib"),
+            date:    "Wed, 18 Sep, 2019 22:30:00 +0000 (GMT)",
+          },
+        ],
       }),
     ])
   }
