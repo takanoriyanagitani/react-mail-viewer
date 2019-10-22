f59df4ae66571c57fe956d5da644c80c3e853702
takanoriyanagitani <takanori.yanagitani@gmail.com>
2019-10-22 15:43:40 +0900

    new example

---
+++ examples/index.js
@@ -0,0 +1,10 @@
+class ReactMailViewer_Ex1 extends React.PureComponent {
+  render(){
+    return React.createElement("div", {}, "hw")
+  }
+}
+
+ReactDOM.render(
+  document.getElementById("root"),
+  React.createElement(ReactMailViewer_Ex1)
+)
