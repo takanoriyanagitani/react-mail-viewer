a12e98e230f0b4f81aba5b9a595a57efa7f3fb28
takanoriyanagitani <takanori.yanagitani@gmail.com>
2019-11-11 05:34:32 +0900

    minimizing...

--- examples/index.js
+++ examples/index.js
@@ -1,25 +1,62 @@
-class ReactMailViewer_Ex1 extends React.PureComponent {
+const sample_mail = {
+  date:    "Sat, 09 Nov 2019 18:41:56 -0800",
+  from:    "takanori.yanagitani@gmail.com",
+  to:      "takanori.yanagitani@gmail.com",
+  subject: "sample subject",
+  content: {
+    type: "text/plain",
+    body: "hw\nthis\nis\nsample\nbody",
+  },
+}
+
+const mail2pairs = ({mail, text2detail, date2detail, content2detail}) => {
+  const {
+    date,
+    from,
+    to,
+    subject,
+    content,
+  } = mail || {}
+  const d2d = date2detail || text2detail
+  const t2d = text2detail
+  const c2d = content2detail
+  return [
+    d2d({term: "Date",    detail: date   }),
+    t2d({term: "From",    detail: from   }),
+    t2d({term: "To",      detail: to     }),
+    t2d({term: "Subject", detail: subject}),
+    c2d({term: "Content", detail: content}),
+  ]
+}
+
+const sample_text2detail = ({term, detail}) => [
+  React.createElement("dt", { key: "term"   }, term  ),
+  React.createElement("dd", { key: "detail" }, detail),
+]
+
+const sample_date2detail = ({term, detail}) => [
+  React.createElement("dt", { key: "term"   }, term  ),
+  React.createElement("dd", { key: "detail" }, new Date(Date.parse(detail)).toLocaleString()),
+]
+
+const sample_content2detail = ({term, detail}) => [
+  React.createElement("dt", { key: "term"   }, term),
+  React.createElement("dd", { key: "detail" }, React.createElement("pre", {}, detail && detail.body)),
+]
+
+class SampleMailViewer extends React.PureComponent {
   render(){
-    const years = [
-      2019,
-      2018,
-      2017,
-      2016,
-      2015,
-      2014,
-      2013,
-      2012,
-      2011,
-    ]
-    const classNames = {
-      cn_years: "list-group",
-      cn_year:  "list-group-item",
-    }
-    return React.createElement("div", {}, React.createElement(ReactMailViewer.Years, { years, classNames }))
+    const { mail } = this.props
+    return React.createElement("dl", {}, mail2pairs({
+      mail,
+      text2detail:    sample_text2detail,
+      date2detail:    sample_date2detail,
+      content2detail: sample_content2detail,
+    }))
   }
 }
 
 ReactDOM.render(
-  React.createElement(ReactMailViewer_Ex1),
+  React.createElement(SampleMailViewer, { mail: sample_mail }),
   document.getElementById("root")
 )
--- test/test.js
+++ test/test.js
@@ -1,114 +1,54 @@
-const React = require("react")
-
-const Enzyme = require("enzyme")
-
 const { expect } = require("chai")
 
-const {
-  shallow,
-  render,
-} = Enzyme
+const ReactMailViewer = require("../lib/react-mail-viewer")
 
-const Adapter = require("enzyme-adapter-react-16")
-
-Enzyme.configure({ adapter: new Adapter() })
-
-const { ReactMailViewer } = require("../lib/react-mail-viewer")
-
-describe("<Year />", () => {
-  const year = 2019
-
-  it("shallow",  () => {
-    const e = React.createElement(ReactMailViewer.Year, { year })
-    expect(shallow(e).contains(React.createElement("li", { className: "ReactMailViewer_Year" }, year)))
-    .to.equal(true)
-  })
-
-  it("render",  () => {
-    expect(render(React.createElement(ReactMailViewer.Year, { year })).text())
-    .to.equal(year+"")
-  })
-})
-
-describe("<Year />(list-group-item)", () => {
-  const year = 2019
-
-  it("shallow",  () => {
-    const cn_year = "list-group-item"
-    const e = React.createElement(ReactMailViewer.Year, { year, cn_year })
-    expect(shallow(e).contains(React.createElement("li", { className: "list-group-item" }, year)))
-    .to.equal(true)
-  })
-})
-
-describe("<Years />", () => {
-  const years = [
-    2019,
-    2018,
-    2017,
-    2016,
-    2015,
-    2014,
-    2013,
-    2012,
-    2011,
-  ]
-
-  it("shallow",  () => {
-    const e = React.createElement(ReactMailViewer.Years, { years })
-    const mapped = years.map((year, key) => React.createElement(ReactMailViewer.Year, { key, year }))
-    expect(shallow(e).contains(React.createElement("ul", { className: "ReactMailViewer_Years" }, mapped)))
-    .to.equal(true)
-  })
-
-  it("render",  () => {
-    expect(render(React.createElement(ReactMailViewer.Years, { years })).text())
-    .to.equal(years.join(""))
-  })
-})
-
-describe("<Years />(list-group, list-group-item)", () => {
-  const years = [
-    2019,
-    2018,
-    2017,
-    2016,
-    2015,
-    2014,
-    2013,
-    2012,
-    2011,
-  ]
-  const classNames = {
-    cn_years: "list-group",
-    cn_year:  "list-group-item",
+const izip = function*(i1, i2){
+  for(const e1 of i1){
+    const next = i2.next()
+    const done = next.done
+    const value = !done && next.value
+    yield [
+      e1,
+      done ? null : value,
+    ]
   }
+}
 
-  const {
-    cn_year,
-    cn_years,
-  } = classNames
-
-  it("shallow",  () => {
-    const e = React.createElement(ReactMailViewer.Years, { years, classNames })
-    const mapped = years.map((year, key) => React.createElement(ReactMailViewer.Year, { key, year, cn_year }))
-    expect(shallow(e).contains(React.createElement("ul", { className: cn_years }, mapped)))
-    .to.equal(true)
-  })
+describe("load", () => {
+  it("loaded",  () => expect(ReactMailViewer && true).to.equal(true))
+  it("version", () => expect(ReactMailViewer && ReactMailViewer.version && true).to.equal(true))
 })
 
-describe("<Years />(empty)", () => {
-  const years = null
-
-  it("shallow",  () => {
-    const e = React.createElement(ReactMailViewer.Years, { years })
-    const mapped = []
-    expect(shallow(e).contains(React.createElement("ul", { className: "ReactMailViewer_Years" }, mapped)))
-    .to.equal(true)
-  })
-
-  it("render",  () => {
-    expect(render(React.createElement(ReactMailViewer.Years, { years })).text())
-    .to.equal("")
+describe("mail2dl", () => {
+  it("loaded", () => expect(ReactMailViewer && ReactMailViewer.mail2dl && true).to.equal(true))
+  it("full",   () => {
+    const mail = {
+      date:    "Sat, 09 Nov 2019 18:41:56 -0800",
+      from:    "takanori.yanagitani@gmail.com",
+      to:      "takanori.yanagitani@gmail.com",
+      subject: "test subject 1",
+      content: {
+        type: "text/plain",
+        body: "helo this is content body.\n",
+      },
+    }
+    const t2d = ({term, detail}) => ([ { tag: "dt", text: term }, { tag: "dd", text: detail             } ])
+    const d2d = ({term, detail}) => ([ { tag: "dt", text: term }, { tag: "dd", text: Date.parse(detail) } ])
+    const c2d = ({term, detail}) => ([ { tag: "dt", text: term }, { tag: "dd", pre:  detail.body        } ])
+    const params = {
+      mail,
+      text2detail:    t2d,
+      date2detail:    d2d,
+      content2detail: c2d,
+    }
+    const o = ReactMailViewer.mail2dl(params)
+    const e = [
+      [{tag: "dt", text: "Date"    }, {tag: "dd", text: 1573353716000}],
+      [{tag: "dt", text: "From"    }, {tag: "dd", text: "takanori.yanagitani@gmail.com"}],
+      [{tag: "dt", text: "To"      }, {tag: "dd", text: "takanori.yanagitani@gmail.com"}],
+      [{tag: "dt", text: "Subject" }, {tag: "dd", text: "test subject 1"}],
+      [{tag: "dt", text: "Content" }, {tag: "dd", pre:  "helo this is content body.\n"}]
+    ]
+    expect(o).to.deep.equal(e)
   })
 })
